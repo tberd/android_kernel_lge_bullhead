a1ab2f671796c032366aa8db6f394ab7a438557d
Paul Moore <pmoore@redhat.com>
2013-07-23 17:38:40 -0400

    selinux: cleanup the XFRM header
    Remove the unused get_sock_isec() function and do some formatting
    fixes.
    
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>
    Change-Id: I2d38919e81713dbda39bc2f17b4fc97f1cd57157

--- security/selinux/include/xfrm.h
+++ security/selinux/include/xfrm.h
@@ -10,8 +10,7 @@
 #include <net/flow.h>
 
 int selinux_xfrm_policy_alloc(struct xfrm_sec_ctx **ctxp,
-			      struct xfrm_user_sec_ctx *uctx,
-			      gfp_t gfp);
+			      struct xfrm_user_sec_ctx *uctx);
 int selinux_xfrm_policy_clone(struct xfrm_sec_ctx *old_ctx,
 			      struct xfrm_sec_ctx **new_ctxp);
 void selinux_xfrm_policy_free(struct xfrm_sec_ctx *ctx);
