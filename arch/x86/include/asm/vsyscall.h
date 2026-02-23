89a38ecb7e3f85e429c926066d51d4da0aa5c3ec
tberd <kotayk@gmx.de>
2026-02-23 09:47:31 +0100

    Initial import

---
+++ arch/x86/include/asm/vsyscall.h
@@ -0,0 +1,15 @@
+#ifndef _ASM_X86_VSYSCALL_H
+#define _ASM_X86_VSYSCALL_H
+
+#include <linux/seqlock.h>
+#include <uapi/asm/vsyscall.h>
+
+extern void map_vsyscall(void);
+
+/*
+ * Called on instruction fetch fault in vsyscall page.
+ * Returns true if handled.
+ */
+extern bool emulate_vsyscall(struct pt_regs *regs, unsigned long address);
+
+#endif /* _ASM_X86_VSYSCALL_H */
